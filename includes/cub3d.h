5287646195cd63ca98eac13b6e65b1555fde1b6d
Duarte Araujo <duartearaujo2002@gmail.com>
2025-08-21 15:31:17 +0100

    Initial commit

---
+++ includes/cub3d.h
@@ -0,0 +1,11 @@
+#ifndef CUB3D_H
+#define CUB3D_H
+
+#include <unistd.h>     // open, close, read, write
+#include <fcntl.h>      // open
+#include <stdio.h>      // printf, perror
+#include <stdlib.h>     // malloc, free, exit
+#include <string.h>     // strerror
+#include <sys/time.h>   // gettimeofday
+
+#endif
