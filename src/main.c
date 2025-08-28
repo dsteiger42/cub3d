dbdd62ec08b22ffdcd9bd50df4c6e4c4881e24c5
gitmini42 <samuelcmcosta@gmail.com>
2025-08-28 18:40:27 +0100

    Add files via upload

--- src/main.c
+++ src/main.c
@@ -3,16 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dsteiger <dsteiger@student.42.fr>          +#+  +:+       +#+        */
+/*   By: scarlos- <scarlos-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/21 16:20:58 by dsteiger          #+#    #+#             */
-/*   Updated: 2025/08/21 16:25:24 by dsteiger         ###   ########.fr       */
+/*   Created: 2025/08/28 16:31:13 by scarlos-          #+#    #+#             */
+/*   Updated: 2025/08/28 18:30:58 by scarlos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
 
-int main()
+int main(int ac, char *av[])
 {
+	t_data data;
 
+	init_data(&data);
+
+    data.map = malloc(sizeof(t_map));
+    if (!data.map)
+    {
+        write(2, "\033[91mERROR\nMemory allocation failed\n", 36);
+        return -1;
+    }
+	init_map(data.map);
+
+	if (ac == 2)
+	{
+		if (valid_map_name(av[1]) == -1 || validfd(data.map, av[1]) == -1 || valid_map(av[1]) == -1)
+			return -1;
+		else
+			return 0;
+	}
+	else
+	{
+		write(2, "\033[91mERROR\nWrong number of args\n", 33);
+		// free aqui
+		return (-1);
+	}
+	//ft_freemap(&img); // free all and exit
+	return (0);
 }
--- src/parsing/check_map.c
+++ src/parsing/check_map.c
@@ -3,11 +3,107 @@
 /*                                                        :::      ::::::::   */
 /*   check_map.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dsteiger <dsteiger@student.42.fr>          +#+  +:+       +#+        */
+/*   By: scarlos- <scarlos-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/21 16:25:15 by dsteiger          #+#    #+#             */
-/*   Updated: 2025/08/21 16:25:34 by dsteiger         ###   ########.fr       */
+/*   Created: 2025/08/28 16:53:43 by scarlos-          #+#    #+#             */
+/*   Updated: 2025/08/28 18:28:15 by scarlos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../includes/cub3d.h"
+#include "../includes/cub3d.h"
+
+int skip_spaces(char c)
+{
+	if (c == 32 || (c >= 9 && c <= 13))
+		return (0);
+	return (1);
+}
+
+int valid_map_name(char *av)
+{
+	int len;
+
+	if (!av)
+	{
+		write(2, "\033[91mERROR\nInvalid pointer\n", 28);
+		return (-1);
+	}
+	len = strlen(av); // mudar pra ft_strlen
+	if (len < 4 || ft_strncmp(&av[len - 4], ".cub", 4) != 0)
+	{
+		write(2, "\033[91mERROR\nMap is not a .cub file\n", 35);
+		return (-1);
+	}
+	return (0);
+}
+int validfd(t_map *map, char *file)
+{
+	int fd;
+	char *line;
+
+	map->line_count = 0;
+	fd = open(file, O_RDONLY);
+	if (fd < 0)
+	{
+		write(2, "\033[91mERROR\nInvalid fd\n", 23);
+		return (-1);
+	}
+	else
+	{
+		line = get_next_line(fd);
+		while (line != NULL)
+		{
+			map->line_count++;
+			free(line);
+			line = get_next_line(fd);
+		}
+		close(fd);
+	}
+	return (0);
+}
+
+int valid_map(char *file)
+{
+	int fd;
+	char *line;
+	int i;
+	char c;
+	int erro = 0;
+
+	fd = open(file, O_RDONLY);
+	if (fd < 0)
+	{
+		write(2, "\033[91mERROR\nInvalid fd\n", 23);
+		return (-1);
+	}
+	line = get_next_line(fd);
+	while (line)
+	{
+		i = 0;
+		while (line[i])
+		{
+			if (!skip_spaces(line[i]))
+			{
+				i++;
+				continue;
+			}
+			c = line[i++];
+			if (c == 'N' || c == 'S' || c == 'W' || c == 'E')
+				erro++;
+			else if (c != '0' && c != '1' && c != '\n')
+			{
+				free(line);
+				write(2, "\033[91mERROR\nInvalid character\n", 30);
+				return (-1);
+			}
+		}
+		free(line);
+		line = get_next_line(fd);
+	}
+	if (erro != 1)
+	{
+		write(2, "\033[91mERROR\nInvalid character\n", 30);
+		return (-1);
+	}
+	return (0);
+}
